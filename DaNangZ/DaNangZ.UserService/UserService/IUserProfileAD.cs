c383fde09e3af9a0968f087e1d250540d2c69b46
ThanhVM <thanhvmse90059@fpt.edu.vn>
2014-12-26 15:00:51 +0700

    Update User Service

---
+++ DaNangZ/DaNangZ.UserService/UserService/IUserProfileAD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaNangZ.UserService.UserService
+{
+    public interface IUserProfileAD<TUserId> : IUserProfile<TUserId>
+        where TUserId : struct
+    {
+        bool IsADUser { get; set; }
+    }
+}
